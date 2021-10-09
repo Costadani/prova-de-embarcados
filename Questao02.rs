5252465c85eb4042cf8291486e232eb00390487f
Costadani <91985209+Costadani@users.noreply.github.com>
2021-10-09 11:46:01 -0400

    Create Questao02.rs

---
+++ Questao02.rs
@@ -0,0 +1,16 @@
+fn main () {
+    deixe mut a = false; // verdadeiro = 1 e falso 0
+    deixe mut x; // Saida
+        
+        println! ("Verdadeiro = 1 Falso = 0 \ n");
+        
+        se um { 
+            x =! (! a);
+            println! ("! (! {}) \ n", x);
+            println! ("{}", x);
+            
+        } outro {
+            x =! (! a); 
+            println! ("{}", x);
+        }
+    }

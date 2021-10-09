fd3482c79e906269119e0f77f54b44545702aa28
Costadani <91985209+Costadani@users.noreply.github.com>
2021-10-09 11:47:58 -0400

    Create Questão05.rs

---
+++ Questão05.rs
@@ -0,0 +1,17 @@
+include <stdio.h>
+
+int main () {
+	Int a = falso; 
+	Int x;			
+	
+	se um){
+		printf ("Saida \ n \ t% d", * (& x));
+		x =! (! a);
+	}outro{
+		printf ("Saida \ n \ t% d", a, * (& x));
+		x =! (! a);
+	}
+
+
+	return 0;
+}

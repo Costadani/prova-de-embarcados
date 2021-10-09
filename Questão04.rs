a9a48170152b968bc5849d4ea798accb4ac4ec73
Costadani <91985209+Costadani@users.noreply.github.com>
2021-10-09 11:47:29 -0400

    Create Questão04.rs

---
+++ Questão04.rs
@@ -0,0 +1,2 @@
+A esquerda temos o código, implementa o código com o algoritmo de exemplo, implementando no microprocessador (SW) onde abre o software e os (HW) trolam hardware que são os circuitos específicos de uma placa de um hardware / software implementação. Uma parte trabalha em (SW) e outra em (HW).
+j
